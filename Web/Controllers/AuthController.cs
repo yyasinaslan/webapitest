bb37a4b7f8319165293fc7885aa8e9eb6f7d31f7
YSN <yyasin.aslan@gmail.com>
2023-07-12 01:49:31 +0300

    Add project files.

---
+++ Web/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
+using Web.Dtos;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult<string> Login(UserLoginDto credentials)
+        {
+            return credentials.Username + '=' + credentials.Password;
+        }
+    }
+}
---
+++ Web/Dtos/UserLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Dtos
+{
+    public class UserLoginDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
